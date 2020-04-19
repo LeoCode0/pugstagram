a5d2452a1ce46691bda26026c31635cd96a5bc65
Leonardo Espinoza <leoespinozidk@gmail.com>
2020-04-19 15:01:05 -0500

    servidor de desarrollo local

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['*', '.mjs', '.js', '.sv']
+        extensions: ['*', '.mjs', '.js', '.svelte']
     },
     module: {
         rules: [
