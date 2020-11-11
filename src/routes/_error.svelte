b9aebc441c92b3439705038274452271cc4788f0
Leonardo Espinoza <leoespinozidk@gmail.com>
2020-11-11 16:29:33 -0600

    ruta de error

---
+++ src/routes/_error.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Main from '../components/Main.svelte';
+  export let status;
+  export let error;
+
+  const dev = process.env.NODE_ENV === 'development';
+</script>
+
+<svelte:head>
+    <title>{status}</title>
+</svelte:head>
+
+<Main>
+  <div class="Error">
+    <h1>{status}</h1>
+    <p>{error.message}</p>
+    {#if dev && error.stack}
+      <pre>{error.stack}</pre>
+    {/if}
+  </div>
+</Main>
