50abbd107bb361c432a927af444a74c92ed72a22
Leonardo Espinoza <leoespinozidk@gmail.com>
2020-11-11 20:07:17 -0600

    Configurando service worker

--- webpack.config.js
+++ webpack.config.js
@@ -65,10 +65,10 @@ module.exports = {
     performance: {
       hints: false,
     },
-    serviceworker: {
-      entry: config.serviceworker.entry(),
-      output: config.serviceworker.output(),
-      mode: process.env.NODE_ENV
-    }
+  },
+  serviceworker: {
+    entry: config.serviceworker.entry(),
+    output: config.serviceworker.output(),
+    mode: process.env.NODE_ENV,
   },
 };
